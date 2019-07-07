41e9f7c2421170219af21485702f119c742b6024
Riley Wilton <meetingprince34@outlook.com>
2019-07-07 19:06:16 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow, Menu, MenuItem} = require('electron') 
+const url = require('url') 
+const path = require('path')  
+
+let win  
+
+function createWindow() { 
+   win = new BrowserWindow({width: 800, height: 600, frame:false, transparent: true}) 
+   win.loadURL(url.format ({ 
+      pathname: path.join(__dirname, 'index.html'), 
+      protocol: 'file:', 
+      slashes: true 
+   })) 
+}
+
+
+app.on('ready', createWindow) 
+ 
