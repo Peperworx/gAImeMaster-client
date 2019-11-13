7ea0023b8b2e6b72e0602b887bdbc1399b6164c2
Riley Wilton <meetingprince34@outlook.com>
2019-11-12 20:20:47 -0500

    Fixed an error and implemented auto git pull

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const { app, BrowserWindow, Menu, MenuItem } = require('electron')
 const url = require('url')
 const path = require('path')
+const simpleGit = require('simple-git');
+
+simpleGit().pull();
 
 let win
 
 function createWindow() {
-    win = new BrowserWindow({ width: 1200, height: 600, 'minWidth': 950, 'minHeight': 600, frame: false, transparent: true })
+    win = new BrowserWindow({ webPreferences: { nodeIntegration: true }, width: 1200, height: 600, 'minWidth': 950, 'minHeight': 600, frame: false, transparent: true })
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'login.html'),
         protocol: 'file:',
