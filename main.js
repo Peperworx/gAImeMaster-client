ee7a5359bf09da0014586335e3c7aebac716680a
Riley Wilton <meetingprince34@outlook.com>
2019-11-30 12:19:56 -0500

    You can now invite players to party with minimum latency and bugs

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow, Menu, MenuItem } = require('electron')
+const electron = require('electron'),
+    ipc = electron.ipcMain;
 const url = require('url')
 const path = require('path')
 const simpleGit = require('simple-git');
@@ -8,13 +10,35 @@ simpleGit().pull();
 let win
 
 function createWindow() {
-    win = new BrowserWindow({ webPreferences: { nodeIntegration: true }, width: 1200, height: 600, 'minWidth': 950, 'minHeight': 600, frame: false, transparent: true })
+    win = new BrowserWindow({ webPreferences: { nodeIntegration: true }, backgroundColor:"#222", width: 1200, height: 600, 'minWidth': 950, 'minHeight': 600, frame: false, transparent: true })
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'login.html'),
         protocol: 'file:',
         slashes: true
     }))
 }
+
+
+
+
+
+ipc.on('close', _ => {
+    status = 1;
+    mainWindow = null;
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+var maximized = false;
+ipc.on('maximize', _ => {
+    maximized = !maximized;
+    win.setFullScreen(maximized);
+});
+
+ipc.on('minimize', _ => {
+    win.minimize();
+});
 
 
 app.on('ready', createWindow)
